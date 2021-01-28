7b7760b4eef9d13e69e691c61db43d75780c689a
SarkarShubhdeep <sarkarshubhdeep1@gmail.com>
2021-01-28 20:24:31 +0530

    added jquery functionalities in add task card [in progress]

--- script/script.js
+++ script/script.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
 
-    $("#plus").click(function(){
-        $("progress").hide();
+    $('#add-subtask').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#add-subtask').before(
+                '<div class="form-check mt-1">' +
+                    '<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">' + 
+                    '<label class="form-check-label" for="defaultCheck1">' + 
+                        $('add-subtask').val() +
+                    '</label>' + 
+                '</div>' 
+            );
+        }
+    });
+    
+
+
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        alert('You pressed a "enter" key in somewhere');    
+    }
+    
+});
+
+// $("#plus").click(function(){
+    //     $("progress").hide();
         
       
-    });
-    $("button").click(function(){
-        $(this).hide();
-    });
-  
-});
+    // });
+    // $("button").click(function(){
+    //     $(this).hide();
+    // });
